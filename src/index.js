6405f2aca9fe7fa6a80c2da94d16b687ff5dee79
sarveshwaran <sarveshsanj2003@gmail.com>
2025-07-12 19:44:45 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals.js';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter} from 'react-router-dom';
 import StoreContextProvider from './context/Context.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
